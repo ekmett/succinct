214d44bdf232389b45981353c5d298ddbba07165
Edward Kmett <ekmett@gmail.com>
2013-11-10 20:02:12 +0100

    INLINE LOUDS

--- src/Succinct/Tree/LOUDS.hs
+++ src/Succinct/Tree/LOUDS.hs
@@ -43,10 +43,12 @@ louds xs = True: False: go 0 where
   go n = case level n xs [] of
     [] -> []
     ys -> ys ++ go (n+1)
+{-# INLINE louds #-}
 
 -- | Convert a finite 'Rose' to 'Rank9'
 fromRose :: Rose -> Rank9
 fromRose = fromList . louds
+{-# INLINE fromRose #-}
 
 level :: Int -> Rose -> [Bool] -> [Bool]
 level 0 (Rose cs) xs = replicate (length cs) True ++ (False:xs)
@@ -62,47 +64,62 @@ data Zipper t = Zipper {-# UNPACK #-} !Int {-# UNPACK #-} !Int t
 
 instance Access a t => Access a (Zipper t) where
   size (Zipper _ _ t) = size t
+  {-# INLINE size #-}
   (!) (Zipper _ _ t) i = t ! i
+  {-# INLINE (!) #-}
 
 instance Dictionary a t => Dictionary a (Zipper t) where
   rank a (Zipper _ _ t) i = rank a t i
+  {-# INLINE rank #-}
   select a (Zipper _ _ t) i = select a t i
+  {-# INLINE select #-}
 
 instance Select0 t => Select0 (Zipper t) where
   select0 (Zipper _ _ t) i = select0 t i
+  {-# INLINE select0 #-}
 
 instance Select1 t => Select1 (Zipper t) where
   select1 (Zipper _ _ t) i = select1 t i
+  {-# INLINE select1 #-}
 
 instance Functor Zipper where
   fmap f (Zipper i j a) = Zipper i j (f a)
+  {-# INLINE fmap #-}
 
 instance Foldable Zipper where
   foldMap f (Zipper _ _ a) = f a
+  {-# INLINE foldMap #-}
 
 instance Traversable Zipper where
   traverse f (Zipper i j a) = Zipper i j <$> f a
+  {-# INLINE traverse #-}
 
 instance Comonad Zipper where
   extract (Zipper _ _ a) = a
+  {-# INLINE extract #-}
   duplicate w@(Zipper i j _) = Zipper i j w
+  {-# INLINE duplicate #-}
 
 -- | The 'root' of our succinct tree.
 root :: t -> Zipper t
 root = Zipper 1 0
+{-# INLINE root #-}
 
 -- | The index of this node in level order, starting at 1 for the root.
 index :: Zipper t -> Int
 index (Zipper i j _) = i - j
+{-# INLINE index #-}
 
 -- | Is this node the 'root'?
 top :: Zipper t -> Bool
 top (Zipper i _ _) = i == 1
+{-# INLINE top #-}
 
 -- | The parent of any node @i /= root@, obtained by a legal sequence of operations.
 parent :: Select1 t => Zipper t -> Zipper t
 parent (Zipper _ j t) = Zipper i' (i' - j) t
   where i' = select1 t j
+{-# INLINE parent #-}
 
 -- | positions of all of the children of a node
 children :: Select0 t => Zipper t -> [Zipper t]
@@ -110,16 +127,19 @@ children (Zipper i j t) = do
   let j' = i - j
   i' <- [select0 t j' + 1..select0 t (j' + 1) - 1]
   return $ Zipper i' j' t
+{-# INLINE children #-}
 
 -- | next sibling, if any
 next :: Access Bool t => Zipper t -> Maybe (Zipper t)
 next (Zipper i j t)
   | i' <- i + 1, t ! i' = Just $ Zipper i' j t
   | otherwise           = Nothing
+{-# INLINE next #-}
 
 -- | Extract a given sub-tree
 rose :: Select0 t => Zipper t -> Rose
 rose i = Rose (rose <$> children i)
+{-# INLINE rose #-}
 
 -- |
 -- @
@@ -127,3 +147,4 @@ rose i = Rose (rose <$> children i)
 -- @
 toRose :: Ranked t => t -> Rose
 toRose = rose . root
+{-# INLINE toRose #-}
