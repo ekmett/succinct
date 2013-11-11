c7f946c7545f74b519aef873aa041509e53f5c0c
Edward Kmett <ekmett@gmail.com>
2013-11-11 14:54:40 +0100

    Added a Dictionary rollup module

---
+++ src/Succinct/Dictionary.hs
@@ -0,0 +1,11 @@
+module Succinct.Dictionary
+  ( module Succinct.Dictionary.Class
+  , module Succinct.Dictionary.Partitioned
+  , module Succinct.Dictionary.Rank9
+  , module Succinct.Dictionary.RangeMin
+  ) where
+
+import Succinct.Dictionary.Class
+import Succinct.Dictionary.Partitioned
+import Succinct.Dictionary.Rank9
+import Succinct.Dictionary.RangeMin
