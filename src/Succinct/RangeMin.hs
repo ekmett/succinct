b132ca48502b55cfd6955e4f6868c30927a7ef08
Edward Kmett <ekmett@gmail.com>
2013-11-11 02:51:53 +0100

    Added RangeMin tree

---
+++ src/Succinct/RangeMin.hs
@@ -0,0 +1,25 @@
+-- TODO: add a Poppy-style 2-level hierarcy
+module Succinct.RangeMin
+  ( RangeMin(..)
+  , fromBits
+  ) where
+
+import Succinct.Dictionary.Class
+import Succinct.Dictionary.Internal.Bit
+import Succinct.RangeMin.Internal.Level
+import Data.Vector as V
+import Data.Vector.Unboxed as U
+import Data.Vector.Primitive as P
+import Data.Word
+
+-- delta rangemin tree
+data RangeMin = RangeMin
+  {-# UNPACK #-} !Int
+  {-# UNPACK #-} !(P.Vector Word64)
+  {-# UNPACK #-} !(V.Vector Level)
+
+instance Bitwise RangeMin where
+  bitwise (RangeMin n bs _) = V_Bit n bs
+
+fromBits :: U.Vector Bit -> RangeMin
+fromBits (V_Bit n bs) = RangeMin n bs (levels bs)
