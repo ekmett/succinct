5bb260113a85435497689cc47b7ddadac719c336
Edward Kmett <ekmett@gmail.com>
2013-11-11 03:51:02 +0100

    ignore "Use camelCase"

--- HLint.hs
+++ HLint.hs
@@ -1,4 +1,5 @@
 import "hint" HLint.HLint
 
-ignore "Eta reduce" -- it is never good efficiency advice
+ignore "Eta reduce"        -- it is never good efficiency advice
 ignore "Redundant bracket" -- CPP isn't good at avoiding prophylactic parens
+ignore "Use camelCase"
