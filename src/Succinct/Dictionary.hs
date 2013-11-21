ec21f1d7a8cb1d94cd99539f9f572e200307ec20
Edward Kmett <ekmett@gmail.com>
2013-11-21 16:47:37 -0500

    Export RRR

--- src/Succinct/Dictionary.hs
+++ src/Succinct/Dictionary.hs
@@ -2,12 +2,14 @@ module Succinct.Dictionary
   ( module Succinct.Dictionary.Builder
   , module Succinct.Dictionary.Class
   , module Succinct.Dictionary.Partitioned
-  , module Succinct.Dictionary.Rank9
   , module Succinct.Dictionary.RangeMin
+  , module Succinct.Dictionary.Rank9
+  , module Succinct.Dictionary.RRR
   ) where
 
 import Succinct.Dictionary.Builder
 import Succinct.Dictionary.Class
 import Succinct.Dictionary.Partitioned
-import Succinct.Dictionary.Rank9
 import Succinct.Dictionary.RangeMin
+import Succinct.Dictionary.Rank9
+import Succinct.Dictionary.RRR
