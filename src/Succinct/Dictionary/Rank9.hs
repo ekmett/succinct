ced65b267e301a92d2a8148bc637f2f4e5130e31
Ben Gamari <bgamari.foss@gmail.com>
2014-08-10 11:43:59 -0400

    Rank9: Parametrize over bitvector type
    This enables the bitvector to be backed by a Storable vector which can
    be disk-backed with an `mmap` mapping.

--- src/Succinct/Dictionary/Rank9.hs
+++ src/Succinct/Dictionary/Rank9.hs
@@ -2,6 +2,9 @@
 {-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Succinct.Dictionary.Rank9
   ( Rank9(..)
@@ -11,37 +14,47 @@ module Succinct.Dictionary.Rank9
 import Control.Applicative
 import Data.Bits
 import qualified Data.Vector.Primitive as P
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Storable as S
 import Data.Vector.Internal.Check as Ck
 import Data.Word
 import Succinct.Dictionary.Builder
 import Succinct.Dictionary.Class
 import Succinct.Internal.Bit as B
+import Succinct.Internal.StorableBitVector as SBV
 import Succinct.Internal.PopCount
 
 #define BOUNDS_CHECK(f) Ck.f __FILE__ __LINE__ Ck.Bounds
 
-data Rank9 = Rank9 {-# UNPACK #-} !Int !(P.Vector Word64) !(P.Vector Word64)
-  deriving (Eq,Ord,Show)
+data Rank9 v = Rank9 {-# UNPACK #-} !Int !(v Word64) !(v Word64)
 
-instance Access Bool Rank9 where
+deriving instance Eq (v Word64) => Eq (Rank9 v)
+deriving instance Ord (v Word64) => Ord (Rank9 v)
+deriving instance Show (v Word64) => Show (Rank9 v)
+
+instance G.Vector v Word64 => Access Bool (Rank9 v) where
   size (Rank9 n _ _) = n
   {-# INLINE size #-}
 
   (!) (Rank9 n bs _) i
      = BOUNDS_CHECK(checkIndex) "Rank9.!" i n
-     $ testBit (P.unsafeIndex bs $ wd i) (bt i)
+     $ testBit (G.unsafeIndex bs $ wd i) (bt i)
   {-# INLINE (!) #-}
 
-instance Bitwise B.Vector Rank9 where
+instance Bitwise B.Vector (Rank9 P.Vector) where
   bitwise (Rank9 n v _) = V_Bit n v
   {-# INLINE bitwise #-}
 
-instance Dictionary Bool Rank9
+instance Bitwise SBV.Vector (Rank9 S.Vector) where
+  bitwise (Rank9 n v _) = SBV.SV_Bit n v
+  {-# INLINE bitwise #-}
 
-instance Select0 Rank9
-instance Select1 Rank9
+instance (G.Vector v Word64, G.Vector v Bool) => Dictionary Bool (Rank9 v)
 
-instance Ranked Rank9 where
+instance (G.Vector v Word64, G.Vector v Bool) => Select0 (Rank9 v)
+instance (G.Vector v Word64, G.Vector v Bool) => Select1 (Rank9 v)
+
+instance (G.Vector v Word64, G.Vector v Bool) => Ranked (Rank9 v) where
   rank1 t@(Rank9 n _ _) i =
     BOUNDS_CHECK(checkIndex) "rank" i (n+1) $
     unsafeRank1 t i
@@ -51,9 +64,9 @@ instance Ranked Rank9 where
     where
       wi = wd i
       block = wi `shiftR` 3 `shiftL` 1
-      base = P.unsafeIndex ps block
+      base = G.unsafeIndex ps block
       t = wi .&. 7 - 1
-      s = P.unsafeIndex ps (block + 1)
+      s = G.unsafeIndex ps (block + 1)
       sShift = (t + t `shiftR` 60 .&. 8) * 9
       count9 = s `unsafeShiftR` sShift .&. 0x1FF
       -- If we just used 'wi' here, we would index out of 'ws' when
@@ -68,19 +81,20 @@ instance Ranked Rank9 where
       -- TODO(klao): Is this needed? How to handle this better?
       -- Abstract it out into Internal!
       wi' = wd (i - 1) - (i - 1) `unsafeShiftR` 63
-      rest = popCountWord64 $ (P.unsafeIndex ws wi') .&. (unsafeBit (bt i) - 1)
+      rest = popCountWord64 $ (G.unsafeIndex ws wi') .&. (unsafeBit (bt i) - 1)
       result = fromIntegral (base + count9) + rest
   {-# INLINE unsafeRank1 #-}
 
-rank9 :: Bitwise B.Vector t => t -> Rank9
+rank9 :: (G.Vector v Word64, Bitwise t v, PackedBits v) => t -> Rank9 (Packed v)
 rank9 t = case bitwise t of
-  v@(V_Bit n ws) -> Rank9 n ws ps
+  v -> Rank9 n (packedBits v) ps
     where
       -- Because we are building word-by-word and not bit-by-bit, we
       -- sometimes build a bigger structure than is strictly
       -- necessary. (In the expression below (n+63) should have been
       -- simply n.)
       k = ((n + 63) `shiftR` 9 + 1) `shiftL` 1
+      n = G.length v
       ps = buildWithFoldlM foldlMPadded (r9Builder $ vectorSized k) v
 {-# INLINE [0] rank9 #-}
 {-# RULES "rank9" rank9 = id #-}
@@ -92,7 +106,8 @@ data Build9 a = Build9
   {-# UNPACK #-} !Word64 -- current "rank9" word
   !a                     -- rank vector builder
 
-r9Builder :: Builder Word64 (P.Vector Word64) -> Builder Word64 (P.Vector Word64)
+r9Builder :: G.Vector v Word64
+          => Builder Word64 (v Word64) -> Builder Word64 (v Word64)
 r9Builder vectorBuilder = Builder $ case vectorBuilder of
   Builder (Building kr hr zr) -> Building stop step start
     where
@@ -109,14 +124,14 @@ r9Builder vectorBuilder = Builder $ case vectorBuilder of
         = stepRank rs tr r9 >>= kr
 {-# INLINE r9Builder #-}
 
-rank9WordBuilder :: Builder Word64 Rank9
+rank9WordBuilder :: G.Vector v Word64 => Builder Word64 (Rank9 v)
 rank9WordBuilder = f <$> vector <*> r9Builder vector
   where
-    f ws rs = Rank9 (P.length ws `shiftL` 6) ws rs
+    f ws rs = Rank9 (G.length ws `shiftL` 6) ws rs
     {-# INLINE f #-}
 {-# INLINE rank9WordBuilder #-}
 
-instance Buildable Bool Rank9 where
+instance G.Vector v Word64 => Buildable Bool (Rank9 v) where
   builder = Builder $ case rank9WordBuilder of
     Builder r9wb -> wordToBitBuilding r9wb fixSize
       where
