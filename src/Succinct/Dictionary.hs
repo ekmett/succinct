04c6ad33c578fcd04da0e2d9c274405e57766edd
Edward Kmett <ekmett@gmail.com>
2015-07-06 15:06:40 -0400

    re-export Poppy

--- src/Succinct/Dictionary.hs
+++ src/Succinct/Dictionary.hs
@@ -2,6 +2,7 @@ module Succinct.Dictionary
   ( module Succinct.Dictionary.Builder
   , module Succinct.Dictionary.Class
   , module Succinct.Dictionary.Partitioned
+  , module Succinct.Dictionary.Poppy
   , module Succinct.Dictionary.RangeMin
   , module Succinct.Dictionary.Rank9
   , module Succinct.Dictionary.RRR
@@ -10,6 +11,7 @@ module Succinct.Dictionary
 import Succinct.Dictionary.Builder
 import Succinct.Dictionary.Class
 import Succinct.Dictionary.Partitioned
+import Succinct.Dictionary.Poppy
 import Succinct.Dictionary.RangeMin
 import Succinct.Dictionary.Rank9
 import Succinct.Dictionary.RRR
