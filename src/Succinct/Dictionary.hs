a272859cc4be46c2939f0f72e697879c7f57bd91
Edward Kmett <ekmett@gmail.com>
2015-07-06 15:05:48 -0400

    Drop the 'as Dict' trick, it confuses haddock

--- src/Succinct/Dictionary.hs
+++ src/Succinct/Dictionary.hs
@@ -1,10 +1,15 @@
 module Succinct.Dictionary
-  ( module Dict
+  ( module Succinct.Dictionary.Builder
+  , module Succinct.Dictionary.Class
+  , module Succinct.Dictionary.Partitioned
+  , module Succinct.Dictionary.RangeMin
+  , module Succinct.Dictionary.Rank9
+  , module Succinct.Dictionary.RRR
   ) where
 
-import Succinct.Dictionary.Builder as Dict
-import Succinct.Dictionary.Class as Dict
-import Succinct.Dictionary.Partitioned as Dict
-import Succinct.Dictionary.RangeMin as Dict
-import Succinct.Dictionary.Rank9 as Dict
-import Succinct.Dictionary.RRR as Dict
+import Succinct.Dictionary.Builder
+import Succinct.Dictionary.Class
+import Succinct.Dictionary.Partitioned
+import Succinct.Dictionary.RangeMin
+import Succinct.Dictionary.Rank9
+import Succinct.Dictionary.RRR
