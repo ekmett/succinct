479326e4d4d041bd0ebc3ff4909578cd76df4f05
Edward Kmett <ekmett@gmail.com>
2015-07-06 20:45:10 -0400

    HLint is an idiot

--- HLint.hs
+++ HLint.hs
@@ -3,3 +3,4 @@ import "hint" HLint.HLint
 ignore "Eta reduce"        -- it is never good efficiency advice
 ignore "Redundant bracket" -- CPP isn't good at avoiding prophylactic parens
 ignore "Use camelCase"
+ignore "Use import/export shortcut" -- this is a _terrible_ suggestion, it breaks the haddocks!
